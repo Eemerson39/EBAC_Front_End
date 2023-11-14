cb6a67f5aec30418082c9a0f07d378f278cbc4b6
Eemerson39 <emerson39@live.com>
2023-11-13 23:05:14 -0300

    correcao do exercicio

--- js/script.js
+++ js/script.js
@@ -14,8 +14,13 @@ function getUser(user) {
     })
         .then((response) => response.json())
         .then((data) => {
-            text += `<p>${data.name} possui ${data.public_repos} repositórios públicos no GitHub como ${data.login}</p>`
-            main.innerHTML = text
+            text = `<p>${data.name} possui ${data.public_repos} repositórios públicos no GitHub como ${data.login}</p>`
+            if (data.name == "" || data.name == undefined){
+                //window.alert("ERRO! Verifique o nome de usuario")
+                main.innerHTML = "<p style=' color: red;'>ERRO!<br> Nome de usuário não encontrado.</p>"
+            } else {
+                main.innerHTML = text
+            }
         })
         .catch((error) => console.error('Erro: ', error.message || error))
 
